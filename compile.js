7b0cad37f543883eb776d97b64a56b72fa451b66
milanjelena <milanrasovic.f@gmail.com>
2019-11-24 23:27:52 +0100

    Adding solc and updating complie file so it compiles our written smart contract

--- compile.js
+++ compile.js
@@ -1,2 +1,12 @@
 //only purpose of this file is to go into contracts folder
 //and compile every contract that is inside of it
+
+const path=require('path');
+const fs=require('fs');
+const solc=require('solc');
+
+const inboxPath=path.resolve(__dirname,'contracts','Inbox.sol');
+const source=fs.readFileSync(inboxPath,'utf8');
+
+module.exports=solc.compile(source,1).contracts[':Inbox'];
+
