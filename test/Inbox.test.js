26d3ea365d63677188c28954b77e9ad303110f77
milanjelena <milanrasovic.f@gmail.com>
2019-11-28 15:49:50 +0100

    Dummy testing 2 assertions with Mocha

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -3,3 +3,26 @@ const ganache=require('ganache-cli');
 const Web3=require('web3');
 //this is instance of Web3 which communicates with ganache local network provider!
 const web3=new Web3(ganache.provider());
+
+class Car{
+    park(){
+        return 'stopped';
+    }
+
+    drive(){
+        return 'vroom';
+    }
+
+}
+
+describe('CarGo',()=>{
+    it('can park',()=>{
+        const car=new Car();
+        assert.equal(car.park(),"stopped");
+    });
+    it('can drive',()=>{
+        const car=new Car();
+        assert.equal(car.drive(),'vroom');
+    })
+})
+
